9dcbd85c7de435a39c7c6b7f8a8aee935ea77293
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2024-10-01 15:38:04 +0200

    fix repo structure

--- src/Q1.py
+++ src/Q1.py
@@ -6,7 +6,7 @@ import os
 import sqlite3
 
 # Path to the SQLite database
-db_file_path = os.path.join( 'data', 'example.db')
+db_file_path = os.path.join( '../data', 'example.db')
 conn = sqlite3.connect(db_file_path)
 
 # Your SQL query goes here
